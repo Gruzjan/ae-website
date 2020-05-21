d903d21a02f3352c23f944ae80f779ea00175d77
Gruzjan <62474802+Gruzjan@users.noreply.github.com>
2020-05-21 20:16:20 +0200

    Add files via upload

---
+++ timer.js
@@ -0,0 +1,19 @@
+window.onload = clock;
+
+function clock()
+	{
+		var today = new Date();
+		
+		var hours = today.getHours();
+		if (hours<10) hours = "0"+hours;
+		
+		var minutes = today.getMinutes();
+		if (minutes<10) minutes = "0"+minutes;
+		
+		var seconds = today.getSeconds();
+		if (seconds<10) seconds = "0"+seconds;
+		
+		document.getElementById("clock").innerHTML = hours+":"+minutes+":"+seconds;
+		 
+		setTimeout("clock()",1000);
+	}
